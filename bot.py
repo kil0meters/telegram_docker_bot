d4f6af924b0500db6790d02c24d2421bcc2d2ba1
kilometers <kilo@meters.sh>
2019-07-08 04:52:21 +0000

    Implement "docker images"

--- bot.py
+++ bot.py
@@ -41,10 +41,10 @@ def ps(bot, update):
 
 def containers(bot, update):
     if update.message.from_user.id in allowed_users:
-        running_containers = docker.containers.list()
+        containers = docker.containers.list()
 
         reply_text = '`ID              NAME            IMAGE               STATUS'
-        for container in running_containers:
+        for container in containers:
             reply_text += '\n{}    {}    {}    {}'.format(container.id[:12],
                                                           container.name[:12].ljust(12),
                                                           container.image.tags[0][:16].ljust(16),
@@ -58,10 +58,42 @@ def containers(bot, update):
                                   'Try adding your user ID `{}` to `ALLOWED_USERNAMES`.'.format(update.message.from_user.id),
                                   parse_mode='markdown')
 
+def images(bot, update):
+    if update.message.from_user.id in allowed_users:
+        images = docker.images.list()
+
+        reply_text = '`ID              REPOSITORY          TAG         SIZE'
+        for image in images:
+            try:
+                repository = image.tags[0].split(':')[0][:16].ljust(16)
+            except:
+                repository = '<none>'.ljust(16)
+
+            try:
+                tag = image.tags[0].split(':')[1][:8].ljust(8)
+            except:
+                tag = '<none>'.ljust(8)
+
+
+            reply_text += '\n{}    {}    {}    {}'.format(image.id[7:][:12],
+                                                          repository,
+                                                          tag,
+                                                          'placeholder')
+        reply_text += '`'
+
+        update.message.reply_text(reply_text,
+                                  parse_mode='markdown')
+    else:
+        update.message.reply_text('It looks like you aren\'t allowed to do that. '
+                                  'Try adding your user ID `{}` to `ALLOWED_USERNAMES`.'.format(update.message.from_user.id),
+                                  parse_mode='markdown')
+
+
 updater.dispatcher.add_handler(CommandHandler('start', help))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('ps', ps))
 updater.dispatcher.add_handler(CommandHandler('containers', containers))
+updater.dispatcher.add_handler(CommandHandler('images', images))
 
 print("Starting bot")
 
