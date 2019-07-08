221f7c221b80340454fff9793b11be6251892dc3
kilometers <kilo@meters.sh>
2019-07-08 01:50:33 +0000

    Implement "docker ps"

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM python:3-slim
 
 WORKDIR /app
 
-COPY . /app
+COPY bot.py /app
+COPY requirements.txt /app
+COPY commands.txt /app
 
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
-ENV API_TOKEN ""
+ENV BOT_TOKEN ""
+ENV ALLOWED_USERS "" 
 
-CMD ["python3", "app.py"]
+CMD ["python3", "bot.py"]
--- bot.py
+++ bot.py
@@ -1,11 +1,49 @@
-import os
+import os, logging
 
-from telegram.ext import Updater
-import logging
+from telegram.ext import Updater, CommandHandler
+import docker as docker_api
 
-token = os.environ['API_TOKEN']
-updater = Updater(token='TOKEN', use_context=True)
+bot_token = os.environ['BOT_TOKEN']
+allowed_users = [int(user_id) for user_id in os.environ['ALLOWED_USERS'].split(',')]
+
+updater = Updater(bot_token)
 dispatcher = updater.dispatcher
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+docker = docker_api.DockerClient(base_url='unix://var/run/docker.sock')
+
+def help(bot, update):
+    reply_text = '*Commands*\n`'
+    
+    with open('commands.txt', 'r') as f:
+        reply_text += f.read()
+        reply_text += '`'
+
+    update.message.reply_text(reply_text,
+                              parse_mode='markdown')
+
+def ps(bot, update):
+    if update.message.from_user.id in allowed_users:
+        running_containers = docker.containers.list(filters={'status': 'running'})
+
+        reply_text = '`ID              NAME            IMAGE'
+        for container in running_containers:
+            reply_text += '\n{}    {}    {}'.format(container.id[:12],
+                                                          container.name[:12].ljust(12),
+                                                          container.image.tags[0][:32].ljust(12))
+        reply_text += '`'
+
+        update.message.reply_text(reply_text,
+                                  parse_mode='markdown')
+    else:
+        update.message.reply_text('It looks like you aren\'t allowed to do that. '
+                                  'Try adding your user ID `{}` to `ALLOWED_USERNAMES`.'.format(update.message.from_user.id),
+                                  parse_mode='markdown')
+
+updater.dispatcher.add_handler(CommandHandler('start', help))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('ps', ps))
+
+print("Starting bot")
+
+updater.start_polling()
+updater.idle()
